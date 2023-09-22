1b759aac1763a8f41b26cd2414ecd2e073ae4e04
majovertiz <maria.jose.vertiz@gmail.com>
2023-09-21 20:23:51 -0700

    Copy my project

---
+++ src/script.js
@@ -0,0 +1,91 @@
+let currentTime = new Date();
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let currentDay = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let currentDate = date.getDate();
+  let currentHours = date.getHours();
+  if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+
+  let todayDate = ` ${currentDay}, ${currentDate} ${currentMonth} ${currentHours}:${currentMinutes} `;
+
+  return todayDate;
+}
+
+let dateToday = document.querySelector("h2");
+dateToday.innerHTML = `${formatDate(currentTime)}`;
+
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let apiKey = "be81f193e065bf5feb2d944c7336968b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "be81f193e065bf5feb2d944c7336968b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let cityForm = document.querySelector("#search-city-form");
+cityForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("San Francisco");
